04058f5bc347ff65ad70d7ab72e619a47f974da7
Omer Yousaf <u.yusaf@gmail.com>
2019-08-07 12:08:13 +0500

    added file missed out in b23ec8b32f3a468c3cb45960be470e38fcd3de4d and updated config.example

--- config.example.js
+++ config.example.js
@@ -1,2 +1,2 @@
-process.env.SERVER_IP = '127.0.0.1';
-process.env.SERVER_PORT = '3001';
+process.env.API_SERVER_URL = '127.0.0.1:8000';
+process.env.LIVE_SERVER_PORT = '8001';
---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CbsFullCalendar from './react.big.calendar/calendar.jsx';
+
+ReactDOM.render(
+  <CbsFullCalendar />, document.getElementById('root')
+);
